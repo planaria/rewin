bbb2dab85ba344f43fea35d42b1e9e140d58ad7d
Suzuki Kojiro <pn.suzuki.kojiro@gmail.com>
2024-10-16 08:50:57 +0900

    backup

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <rewin/rewin.hpp>
 
 #include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
@@ -13,7 +14,17 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmd
 		auto text_box = rewin::text_box_view::create();
 		text_box->allow_enter() = true;
 
-		window->contents() = { text_box };
+		auto scroll_view = rewin::scroll_view::create();
+		scroll_view->direction() = rewin::direction::bottom;
+		scroll_view->contents() = { text_box };
+
+		window->contents() = { scroll_view };
+
+		window->title() = text_box->rect_arranged_vertical().as_reactive().map(
+			[](const auto& t)
+		{
+			return boost::lexical_cast<std::wstring>(t.left);
+		});
 
 		rewin::unsubscriber us;
 
